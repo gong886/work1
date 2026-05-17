d42b01487e93048f11335ee1f2cf2da0484e80ca
gong886 <912672707@qq.com>
2026-05-18 01:10:36 +0800

    B2 update app behavior

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 def main():
-    print("Project management experiment B1")
+    print("Project management experiment on B2 branch")
+    print("B2 focuses on local implementation changes.")
 
 if __name__ == "__main__":
     main()
